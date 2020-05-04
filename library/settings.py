4fa7c9ab4fb3d259c02fb17e890fee73938bcfec
Yash jain <jainism987e@gmail.com>
2020-05-04 20:57:25 +0530

    some change

--- library/settings.py
+++ library/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '==e^87*tib$+f)9$46#-jc&toydks5v^g5ym7wq^shf5(hno3&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
